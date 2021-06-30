07c001bf7e016218501bc91489a6e9bf3f28702a
Konrad <konrad1jasinski@gmail.com>
2021-07-01 01:39:56 +0200

    added ads

--- app/src/main/java/com/thyme/eatandrun/MainActivity.kt
+++ app/src/main/java/com/thyme/eatandrun/MainActivity.kt
@@ -2,12 +2,16 @@ package com.thyme.eatandrun
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.thyme.eatandrun.auth.AuthActivity
@@ -42,6 +46,28 @@ class MainActivity : AppCompatActivity(), OverviewFragment.OnOverviewCurrent {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupNavigationView()
+        window.setSoftInputMode(
+            WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+
+        // Initialize the Mobile Ads SDK with an AdMob App ID.
+        MobileAds.initialize(this) {}
+
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("ABCDEF012345"))
+                .build()
+        )
+
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading the ad in the background.
+
 
         mAuth = FirebaseAuth.getInstance()
 
@@ -58,6 +84,8 @@ class MainActivity : AppCompatActivity(), OverviewFragment.OnOverviewCurrent {
 
         val incomingIntent = intent
         _selectedDate = incomingIntent.getStringExtra("date")
+
+        binding.adView.loadAd(adRequest)
 
     }
 
@@ -93,4 +121,21 @@ class MainActivity : AppCompatActivity(), OverviewFragment.OnOverviewCurrent {
         return mNavController.navigateUp(mAppBarConfiguration) || super.onSupportNavigateUp()
     }
 
+    // Called when leaving the activity
+    public override fun onPause() {
+        binding.adView.pause()
+        super.onPause()
+    }
+
+    // Called when returning to the activity
+    public override fun onResume() {
+        super.onResume()
+        binding.adView.resume()
+    }
+
+    // Called before the activity is destroyed
+    public override fun onDestroy() {
+        binding.adView.destroy()
+        super.onDestroy()
+    }
 }
